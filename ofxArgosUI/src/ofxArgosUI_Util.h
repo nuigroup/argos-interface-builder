d2ad5d689fe4d64e0a7cde0900aa94df6a48a17d
eliftherious <eliftherious@85a45124-5891-11de-8d7d-01d876f1962d>
2009-08-28 18:59:40 +0000

    new util file
    git-svn-id: http://ofxargos.googlecode.com/svn/trunk@56 85a45124-5891-11de-8d7d-01d876f1962d

---
+++ ofxArgosUI/src/ofxArgosUI_Util.h
@@ -0,0 +1,80 @@
+/***********************************************************************
+ 
+ Copyright (c) 2009 Dimitri Diakopoulos, http://www.dimitridiakopoulos.com/
+ === Google Summer of Code 2009 - NUI Group === 
+
+ Portions Copyright (c) 2008, 2009 Memo Atkens, http://www.memo.tv/
+ -> Based on ofxSimpleGuiToo
+ 
+ Portions Copyright (c) 2008 Todd Vanderlin, http://toddvanderlin.com/
+ -> Inspired by ofxSimpleGui API
+
+	Redistribution and use in source and binary forms, with or without modification, 
+	are permitted provided that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice, 
+	this list of conditions and the following disclaimer.
+
+	2. Redistributions in binary form must reproduce the above copyright notice, 
+	this list of conditions and the following disclaimer in the documentation and/or 
+	other materials provided with the distribution.
+
+	3. The name of the author may not be used to endorse or promote products derived 
+	from this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+	ARE DISCLAIMED. IN NOEVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, 
+	INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+	(INCLUDING, BUT NOT LIMITED TO,PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+	LOSS OF USE, DATA, ORPROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+	AND ON ANY THEORY OFLIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+	OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+	OF THE USE OF THIS SOFTWARE,EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+
+*************************************************************************/ 
+#pragma once
+
+inline void rRectangle(int x, int y, int w, int h, int radius){
+
+    glDisable(GL_TEXTURE_2D);
+
+    glBegin(GL_POLYGON);
+
+    glVertex2f (x + radius, y);
+    glVertex2f (x + w - radius, y);
+		for(float t  = PI * 1.5f; t < PI * 2; t += 0.1f){
+			float sx = x + w - radius + cos(t) * radius;
+			float sy = y + radius + sin(t) * radius;
+			glVertex2f (sx, sy);
+		}
+
+    glVertex2f (x + w, y + radius);
+    glVertex2f (x + w, y + h - radius);
+		for(float t  = 0; t < PI * 0.5f; t += 0.1f){
+			float sx = x + w - radius + cos(t) * radius;
+			float sy = y + h - radius + sin(t) * radius;
+			glVertex2f (sx, sy);
+		}
+        
+    glVertex2f (x + w -radius, y + h);
+    glVertex2f (x + radius, y + h);
+		for(float t  = PI * 0.5f; t < PI; t += 0.1f){
+			float sx = x + radius + cos(t) * radius;
+			float sy = y + h - radius + sin(t) * radius;
+			glVertex2f (sx, sy);
+		}
+    
+    glVertex2f (x, y + h - radius);
+    glVertex2f (x, y + radius);
+		for(float t  = PI; t < PI * 1.5f; t += 0.1f){
+			float sx = x + radius + cos(t) * radius;
+			float sy = y + radius + sin(t) * radius;
+			glVertex2f (sx, sy);
+		}
+    
+    glEnd();
+
+    glEnable(GL_TEXTURE_2D);    
+}
